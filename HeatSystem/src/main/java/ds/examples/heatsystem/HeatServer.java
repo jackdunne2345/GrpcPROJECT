62a754aac972f40d711b293993141c45ad73c5bd
jackdunne2345 <34764297+jackdunne2345@users.noreply.github.com>
2022-03-26 22:24:43 +0000

    start
    start

---
+++ HeatSystem/src/main/java/ds/examples/heatsystem/HeatServer.java
@@ -0,0 +1,10 @@
+package ds.examples.heatsystem;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+import java.io.IOException;
+
+
+public class HeatServer {
+	public static void main(String[] args) {
+		
+	}
+}
